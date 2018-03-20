be3bd7423e7214f4ae822d598974e919ad9b0d89
alexei andrusceac <alexeiandrusceac@hotmail.com>
2018-03-20 19:44:15 +0200

    56

--- app/src/main/java/com/example/alexei/monitorizare/MonitorizareMainActivity.java
+++ app/src/main/java/com/example/alexei/monitorizare/MonitorizareMainActivity.java
@@ -14,17 +14,14 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.DatePicker;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
-
-import org.w3c.dom.Text;
 
 import java.util.Calendar;
 
-
+/////////akjfhbshbdhfbvshfbdshfbsbfdbfhbs
 public class MonitorizareMainActivity extends AppCompatActivity {
         DatePickerDialog datepicker;
-        DataBaseHelper mydbHelper = new DataBaseHelper(this.getApplicationContext());
+
         final Context mContext = this;
         private EditText dateOuput;
         private EditText primitOutput;
@@ -50,14 +47,15 @@ public class MonitorizareMainActivity extends AppCompatActivity {
                 View dialogView = li.inflate(R.layout.data_layout, null);
                 AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(
                         mContext);
+                // set title
+                alertDialogBuilder.setTitle("Custom Dialog");
+                // set custom dialog icon
                 //alertDialogBuilder.setIcon(R.drawable.ic_launcher);
                 // set custom_dialog.xml to alertdialog builder
                 alertDialogBuilder.setView(dialogView);
                 final EditText primitInput = (EditText)findViewById(R.id.inputText);
                 final EditText cheltuitInput = (EditText)findViewById(R.id.outputText);
                 final  EditText dateInput  =  setDate(dialogView);
-               // final TextView differenceInput = (TextView)findViewById(R.id.differenceText);
-
 
                 // set dialog message
                 alertDialogBuilder
@@ -68,19 +66,9 @@ public class MonitorizareMainActivity extends AppCompatActivity {
                                                         int id) {
                                         // get user input and set it to etOutput
                                         // edit text
-                                        int primitValue = 0;
-                                        int cheltuitValue = 0;
-
-                                        int difference = 0;
-                                        primitValue =  Integer.parseInt(primitInput.getText().toString());
-                                        cheltuitValue = Integer.parseInt(cheltuitInput.getText().toString());
-
-                                        difference = primitValue - cheltuitValue;
-
-                                        mydbHelper.insertData(dateInput.getText().toString(),primitValue,cheltuitValue,difference,0,0);
-                                        /*dateOuput.setText(dateInput.getText());
+                                        dateOuput.setText(dateInput.getText());
                                         primitOutput.setText(primitInput.getText());
-                                        cheltuitOutput.setText(cheltuitInput.getText());*/
+                                        cheltuitOutput.setText(cheltuitOutput.getText());
 
                                     }
                                 })
